5275873006345fbbb130a1ef2c684ea919d71ec1
Naomi-ANN <nevellalex@gmail.com>
2025-08-26 15:09:44 -0700

    Create class.cpp

---
+++ Classwork/class.cpp
@@ -0,0 +1,32 @@
+#include <iostream>// without it computer wouldnt understand cout 
+
+/*block 
+of 
+comments 
+*/
+
+using namespace std;// helps computer so we dont have to add the std before  std::cout<"hello world";
+
+int main ()
+{
+    int value = 10;
+
+    cout << value << endl;// how to pritn my value 
+
+    //mod checks if a value is even 
+
+    int mod = value%2;
+
+    cout <<"My value is even if it prints zero"<<mod<<endl; 
+
+    //1=,==(comparsion), =(equal operator),
+
+
+    cout << "make your own value"<<
+    int user;
+    cin >> user >>; 
+    cout << "your value is"<< user<<endl;
+
+    return 0;
+    
+}
